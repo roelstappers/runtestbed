b6c3abcf554fe7d3e8a1d7917c8a7c9d90aabffa
Roel Stappers <roels@met.no>
2022-02-09 18:06:37 +0100

    Update expect.sh

--- .github/workflows/expect.sh
+++ .github/workflows/expect.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/expect -f
 
 spawn ssh ec -l $USER
-expect "*password: "
-send "$HID\r"
+expect "*password: " {send "$HID\r"}
 expect "*: " {send "\r"}
 expect "*$ " {send "touch blabla3\r"}
 expect "*$ " {send "exit\r"}
